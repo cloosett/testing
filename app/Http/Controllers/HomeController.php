ab90993b306cbf977bbd27972c3d41a4e1e51ac5
maks <samper.goss@gmail.com>
2024-12-18 19:10:08 +0200

    + remove cache

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,16 +10,8 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $votesIgor = Cache::get('votes_igor');
-        $votesSidjey = Cache::get('votes_sidjey');
-
-        if (!$votesIgor || !$votesSidjey) {
-            $votesIgor = Votes::where('vote', 'igor')->count();
-            $votesSidjey = Votes::where('vote', 'sidjey')->count();
-
-            Cache::put('votes_igor', $votesIgor, now()->addMinutes(10));
-            Cache::put('votes_sidjey', $votesSidjey, now()->addMinutes(10));
-        }
+        $votesIgor = Votes::where('vote', 'igor')->count();
+        $votesSidjey = Votes::where('vote', 'sidjey')->count();
 
         return view('welcome', compact('votesIgor', 'votesSidjey'));
     }
